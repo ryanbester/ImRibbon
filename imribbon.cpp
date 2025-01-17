3e25e005e3370e8be41e34d0f0e75b6c7847644a
Ryan Bester <ryan.bester5@gmail.com>
2025-01-17 11:40:35 +0000

    Add windowing code for Win32.

--- imribbon.cpp
+++ imribbon.cpp
@@ -19,8 +19,91 @@ namespace ImRibbon {
 
 #pragma region Windowing
 
-    void SetBorderlessWindow(bool borderless) {
+#ifdef WIN32
+    static long long old_wnd_proc;
 
+    static LRESULT borderlessWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+        switch (uMsg) {
+            case WM_NCACTIVATE:
+            case WM_NCPAINT:
+                // Stop curving corners on window restore
+                return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+            case WM_NCCALCSIZE: {
+                // Window resizing
+                RECT &rect = *reinterpret_cast<RECT *>(lParam);
+                RECT client = rect;
+
+                CallWindowProc(reinterpret_cast<WNDPROC>(old_wnd_proc), hwnd, uMsg, wParam, lParam);
+
+                if (IsMaximized(hwnd)) {
+                    WINDOWINFO window_info = {0};
+                    window_info.cbSize = sizeof(WINDOWINFO);
+                    GetWindowInfo(hwnd, &window_info);
+
+                    rect = RECT{
+                            .left = static_cast<LONG>(client.left + window_info.cyWindowBorders),
+                            .top = static_cast<LONG>(client.top + window_info.cyWindowBorders),
+                            .right = static_cast<LONG>(client.right - window_info.cyWindowBorders),
+                            .bottom = static_cast<LONG>(client.bottom - window_info.cyWindowBorders)
+                    };
+                } else {
+                    rect = client;
+                }
+
+                return WVR_REDRAW;
+            }
+            case WM_NCHITTEST: {
+                // Handle window resizing and moving
+                POINT cursor = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+
+                const POINT border{
+                        static_cast<LONG>((GetSystemMetrics(SM_CXFRAME) + GetSystemMetrics(SM_CXPADDEDBORDER))),
+                        static_cast<LONG>((GetSystemMetrics(SM_CYFRAME) + GetSystemMetrics(SM_CXPADDEDBORDER)))
+                };
+
+                RECT window;
+                if (!GetWindowRect(hwnd, &window)) {
+                    return HTNOWHERE;
+                }
+
+                bool top_border = cursor.y <= (window.top + border.y);
+                bool bottom_border = cursor.y >= (window.bottom - border.y);
+                bool left_border = cursor.x <= (window.left + border.x);
+                bool right_border = cursor.x >= (window.right - border.x);
+
+                // Corners
+                if (top_border && left_border) return HTTOPLEFT;
+                if (top_border && right_border) return HTTOPRIGHT;
+                if (bottom_border && left_border) return HTBOTTOMLEFT;
+                if (bottom_border && right_border) return HTBOTTOMRIGHT;
+
+                // Edges
+                if (top_border) return HTTOP;
+                if (bottom_border) return HTBOTTOM;
+                if (left_border) return HTLEFT;
+                if (right_border) return HTRIGHT;
+
+                // Title bar bounds checking
+                if ((float) cursor.y >= ((float) window.top + GImRibbon.TitleBarBoundsMin.y)
+                    && (float) cursor.y <= ((float) window.top + GImRibbon.TitleBarBoundsMax.y)) {
+                    if ((float) cursor.x >= ((float) window.left + GImRibbon.TitleBarBoundsMin.x)
+                        && (float) cursor.x <= ((float) window.left + GImRibbon.TitleBarBoundsMax.x)) {
+                        return HTCAPTION;
+                    }
+                }
+
+                return HTCLIENT;
+            }
+            default:
+                break;
+        }
+
+        return CallWindowProc(reinterpret_cast<WNDPROC>(old_wnd_proc), hwnd, uMsg, wParam, lParam);
+    }
+
+#endif
+
+    void SetBorderlessWindow(bool borderless) {
 #ifdef IMRIBBON_GLFW
         glfwWindowHint(GLFW_DECORATED, borderless ? GLFW_FALSE : GLFW_TRUE);
 #endif
